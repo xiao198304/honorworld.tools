18aff728b2a60f8c865bd5cbe7878c63636d5518
xiao198304 <386590727@qq.com>
2022-12-31 21:44:49 +0800

    fix some farm

---
+++ assets/hero.a5dbdd96.js
@@ -0,0 +1,1 @@
+import{J as r,K as t}from"./index.fae82586.js";import{d as e}from"./honor.a54e0f9f.js";const s="https://tools-1258577806.cos.ap-hongkong.myqcloud.com/honorworld/nftmarket/floorprice.json",c=r("hero",{state:()=>({floorprice:[]}),actions:{async initFloorprice(){let o=s+"?="+e().format("YYYYMMDDHHMM");this.floorprice=await t(o)}}}),n=c;export{n as u};
