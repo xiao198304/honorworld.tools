a0280acefde84d7d0ace1e4f6f614aef98fab439
dfsfans <8671056+dfsfans@user.noreply.gitee.com>
2023-01-05 16:29:06 +0800

    update scf

---
+++ assets/index.22595c73.js
@@ -0,0 +1,1 @@
+import{F as o,G as t,H as p,I as s}from"./index.099fc90d.js";const m=o({__name:"index",setup(a){return(e,c)=>{const n=p("van-empty");return s(),t(n,{description:e.$t("common.developing")},null,8,["description"])}}});export{m as default};
