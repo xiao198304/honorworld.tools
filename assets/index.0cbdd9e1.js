53976bbaeb6beef3db60573b930542abdafabf20
dfsfans <8671056+dfsfans@user.noreply.gitee.com>
2023-01-13 11:53:26 +0800

    up vipinfo page

---
+++ assets/index.0cbdd9e1.js
@@ -0,0 +1,1 @@
+import{_ as o,G as t,H as c,I as r}from"./index.b32be5f8.js";const s={};function a(e,p){const n=c("van-empty");return r(),t(n,{description:e.$t("common.developing")},null,8,["description"])}const i=o(s,[["render",a]]);export{i as default};
